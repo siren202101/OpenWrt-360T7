c44119b68a7200eb24769f384d3655b08286fbcc
siren202101 <80493699+siren202101@users.noreply.github.com>
2024-09-17 22:58:13 +0800

    Delete ipk.sh

--- ipk.sh
+++
@@ -1,2 +0,0 @@
-sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git-full siren202101 https://github.com/siren202101/AdGuardHome-Cluster.git' feeds.conf.default
