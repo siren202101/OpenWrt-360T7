9a7391dafd9231e5a97e74ca61e410677035f0ee
siren202101 <80493699+siren202101@users.noreply.github.com>
2024-09-17 19:37:02 +0800

    Update ipk.sh

--- ipk.sh
+++ ipk.sh
@@ -1,1 +1,1 @@
-sed -i '$a src-git-full AdGuardHome https://github.com/siren202101/AdGuardHome-Cluster.git' feeds.conf.default
+sed -i '$a src-git-full siren202101 https://github.com/siren202101/AdGuardHome-Cluster.git' feeds.conf.default
