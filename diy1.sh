e4ac4a276dd304845f9f3061f2230583edc54af9
siren202101 <80493699+siren202101@users.noreply.github.com>
2025-04-14 15:07:10 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -15,8 +15,11 @@
 
 # Add feed sources
 sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git-full kenzok https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git-full xiaorouji_passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 sed -i '$a src-git-full sbwml_mosdns https://github.com/sbwml/luci-app-mosdns.git' feeds.conf.default
+
+
 
 git clone https://github.com/MetaCubeX/subconverter.git package/subconverter
 
