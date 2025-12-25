8cd9e667e0831a203788171c9db8c16cdb5551fe
siren202101 <80493699+siren202101@users.noreply.github.com>
2025-12-26 02:03:32 +0800

    Create imm.diy2.sh

---
+++ imm.diy2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+###
+ # @Author: xiawang1024
+ # @Date: 2023-01-06 15:29:51
+ # @LastEditTime: 2023-01-06 15:30:17
+ # @LastEditors: xiawang1024
+ # @Description: 
+ # @FilePath: /OpenWrt-360T7/diy2.sh
+ # 工作，生活，健康
+### 
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.6.1/192.168.88.7/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.7/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.7/g' ppackage/base-files/luci2/bin/config_generate
+#sed -i "s/hostname='ImmortalWrt'/hostname='OpenWRT-360T7'/g" package/base-files/files/bin/config_generate
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By 神棍 $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
