55f536cf349d98dd4cfaddefe72c176f01a229b6
siren202101 <80493699+siren202101@users.noreply.github.com>
2026-01-02 10:41:21 +0800

    Update imm.diy2.sh

--- imm.diy2.sh
+++ imm.diy2.sh
@@ -26,10 +26,19 @@ sed -i 's/192.168.1.1/192.168.88.7/g' ppackage/base-files/luci2/bin/config_gener
 #sed -i "s/hostname='ImmortalWrt'/hostname='OpenWRT-360T7'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By 神棍 $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
 
+# =========================================================
 # 强制内置 MT7981 + MT7976 DBDC EEPROM（360T7 必需）
+# =========================================================
+
 EEP_DIR="target/linux/mediatek/files/lib/firmware/mediatek"
 
-mkdir -p ${EEP_DIR}
+mkdir -p "${EEP_DIR}"
 
-wget -O ${EEP_DIR}/mt7981_eeprom_mt7976_dbdc.bin \
+wget -q -O "${EEP_DIR}/mt7981_eeprom_mt7976_dbdc.bin" \
 https://raw.githubusercontent.com/openwrt/mt76/eb567bc7f9b692bbf1ddfe31dd740861c58ec85b/firmware/mt7981_eeprom_mt7976_dbdc.bin
+
+# 简单校验，避免下载失败却继续编译
+[ -s "${EEP_DIR}/mt7981_eeprom_mt7976_dbdc.bin" ] || {
+    echo "ERROR: mt7981_eeprom_mt7976_dbdc.bin download failed"
+    exit 1
+}
